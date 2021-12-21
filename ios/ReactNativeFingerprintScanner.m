9661bcf61bdcbf67779bb1d3da01ae9df5ca7b12
Alireza Yusefi <rayansys.rs@gmail.com>
2021-12-21 11:53:57 +0330

    Update ReactNativeFingerprintScanner.m

--- ios/ReactNativeFingerprintScanner.m
+++ ios/ReactNativeFingerprintScanner.m
@@ -64,11 +64,11 @@ RCT_EXPORT_METHOD(authenticate: (NSString *)reason
     if (!fallbackEnabled) {
         context.localizedFallbackTitle = @"";
     }
-
+    __auto_type policy = fallbackEnabled ? LAPolicyDeviceOwnerAuthentication : LAPolicyDeviceOwnerAuthenticationWithBiometrics
     // Device has FingerprintScanner
-    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+    if ([context canEvaluatePolicy:policy error:&error]) {
         // Attempt Authentication
-        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
+        [context evaluatePolicy:policy
                 localizedReason:reason
                           reply:^(BOOL success, NSError *error)
          {
